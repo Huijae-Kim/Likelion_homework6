cbbacdb7a630bb63d5e12d2cf5e035851e90b591
HUIJAE KIM <heejae6021@gmail.com>
2017-05-16 04:00:37 +0000

    change login system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,28 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:email_nickname])
-    user = User.find_by(nickname: params[:email_nickname])
     
-    if user && user.authenticate(params[:password])
-      #로그인 성공했을 때
-      session[:user_id] = user.id
-      redirect_to contents_path
-    else
-      #로그인 실패했을 때
-      flash[:alert] = 'login error'
-      render 'new' #sessions#new로 갈 것. 로그인창으로 돌아간다.
+    if user = User.find_by(nickname: params[:email_nickname])
+      if user && user.authenticate(params[:password])
+        #로그인 성공했을 때
+        session[:user_id] = user.id
+        redirect_to contents_path
+      else
+        #로그인 실패했을 때
+        flash[:alert] = 'login error'
+        render 'new' #sessions#new로 갈 것. 로그인창으로 돌아간다.
+      end
+
+    elsif user = User.find_by(email: params[:email_nickname])
+      if user && user.authenticate(params[:password])
+        #로그인 성공했을 때
+        session[:user_id] = user.id
+        redirect_to contents_path
+      else
+        #로그인 실패했을 때
+        flash[:alert] = 'login error'
+        render 'new' #sessions#new로 갈 것. 로그인창으로 돌아간다.
+      end
     end
   end
   
