93e49a700bd5169b57112f9c69fb9473c2dda633
HUIJAE KIM <heejae6021@gmail.com>
2017-05-16 10:59:26 +0000

    final

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   
   def create
     
-    if user = User.find_by(nickname: params[:email_nickname])
+  user = User.find_by(nickname: params[:email_nickname]) || User.find_by(email: params[:email_nickname])
       if user && user.authenticate(params[:password])
         #로그인 성공했을 때
         session[:user_id] = user.id
@@ -15,18 +15,6 @@ class SessionsController < ApplicationController
         flash[:alert] = 'login error'
         render 'new' #sessions#new로 갈 것. 로그인창으로 돌아간다.
       end
-
-    elsif user = User.find_by(email: params[:email_nickname])
-      if user && user.authenticate(params[:password])
-        #로그인 성공했을 때
-        session[:user_id] = user.id
-        redirect_to contents_path
-      else
-        #로그인 실패했을 때
-        flash[:alert] = 'login error'
-        render 'new' #sessions#new로 갈 것. 로그인창으로 돌아간다.
-      end
-    end
   end
   
   def destroy
